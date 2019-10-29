b5eb3c1cc20152938e6fc6e216e100d4b2ff9776
vidbh <vidbh@DESKTOP-3PG1MME>
2019-10-29 13:35:58 +0400

    Removed exit button

--- BatteryMonitor/Form1.cs
+++ BatteryMonitor/Form1.cs
@@ -15,6 +15,8 @@ namespace BatteryMonitor
         public Form1()
         {
             InitializeComponent();
+            label2.Text = GetBatteryStatus().ToString() + '%';
+            label4.Text = GetPowerSource();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -32,9 +34,41 @@ namespace BatteryMonitor
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       // private void button1_Click(object sender, EventArgs e)
+       // {
+       //     Application.Exit();
+       // }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Application.Exit();
+
         }
+
+        public String GetPowerSource()
+        {
+            string strPowerLineStatus = "Default";
+            // Getting the current system power status.
+            switch (SystemInformation.PowerStatus.PowerLineStatus)
+            {
+                case PowerLineStatus.Offline:
+                    strPowerLineStatus = "Battery";
+                    break;
+                case PowerLineStatus.Online:
+                    strPowerLineStatus = "AC Power";
+                    break;
+                case PowerLineStatus.Unknown:
+                    strPowerLineStatus = "Unknown";
+                    break;
+            }
+            return strPowerLineStatus;
+        }
+        public float GetBatteryStatus()
+        {
+            float batterylife;
+            batterylife = SystemInformation.PowerStatus.BatteryLifePercent;
+            batterylife *= 100.0f;
+            return batterylife;
+        }
+
     }
 }
